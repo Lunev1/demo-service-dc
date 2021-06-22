9879cedd612ebc8b69c6977c43fab0d6c77c136a
Florian Zeidler <f.zeidler@finatix.de>
2021-06-22 13:26:15 +0200

    DEMO init

---
+++ src/main/java/de/finatix/demoservicedc/DemoServiceDcApplication.java
@@ -0,0 +1,13 @@
+package de.finatix.demoservicedc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoServiceDcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoServiceDcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/finatix/demoservicedc/DemoServiceDcApplicationTests.java
@@ -0,0 +1,13 @@
+package de.finatix.demoservicedc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoServiceDcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
