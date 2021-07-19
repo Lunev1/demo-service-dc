b22ec004bfe04da5aaed60d7a608ca53613253f3
Florian Zeidler <f.zeidler@finatix.de>
2021-07-20 00:39:26 +0200

    added logging, cleanup

--- src/main/java/de/finatix/demoservicedc/controller/DatabaseController.java
+++ src/main/java/de/finatix/demoservicedc/controller/DatabaseController.java
@@ -2,6 +2,8 @@ package de.finatix.demoservicedc.controller;
 
 import de.finatix.demoservicedc.database.model.Register;
 import de.finatix.demoservicedc.database.repository.RegisterRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,13 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DatabaseController {
 
+    Logger logger = LoggerFactory.getLogger(DatabaseController.class);
+
     @Autowired
     private RegisterRepository registerRepository;
-
-    @GetMapping("/greeting")
-    public Register getRegister() {
-        return new Register();
-    }
 
     @PostMapping(path = "/add")
     public @ResponseBody
@@ -27,13 +26,14 @@ public class DatabaseController {
         n.setName(name);
         n.setType(type);
         registerRepository.save(n);
+        logger.info(String.format("dataset added: %s, %s", name, type));
         return "Saved successfully";
     }
 
     @GetMapping(path = "/all")
     public @ResponseBody
     Iterable<Register> getAllRegister() {
-        // This returns a JSON or XML with the registers
+        logger.info("all registered datasets requested");
         return registerRepository.findAll();
     }
 }
